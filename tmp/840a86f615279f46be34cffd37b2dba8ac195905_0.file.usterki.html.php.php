b444076067f0d7eab5cd65ebf5c45ee798398f33
Szymon Motłoch <01122378@pw.edu.pl>
2020-02-02 11:30:07 +0100

    Front - end

---
+++ tmp/840a86f615279f46be34cffd37b2dba8ac195905_0.file.usterki.html.php.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 3.1.34-dev-7, created on 2020-02-01 22:06:47
+  from 'C:\xampp\htdocs\wspolnota-mieszkaniowa\templates\usterki\usterki.html.php' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.34-dev-7',
+  'unifunc' => 'content_5e35e867d82cc3_33574720',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '840a86f615279f46be34cffd37b2dba8ac195905' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\wspolnota-mieszkaniowa\\templates\\usterki\\usterki.html.php',
+      1 => 1580591030,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5e35e867d82cc3_33574720 (Smarty_Internal_Template $_smarty_tpl) {
+?>lista usterek
+
+<?php }
+}
